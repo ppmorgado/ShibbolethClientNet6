e05a0f59bda927812b3c4df40f99eadd24a49194
Paulo Morgado <paulo.morgado@brightpartners.com>
2024-04-15 21:30:20 +0100

    initial

---
+++ ShibClient2/Program.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ShibClient2.Data;
+using UW.AspNetCore.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(
+//                                                      options =>
+//                                                      {
+//                                                          options.SignIn.RequireConfirmedAccount = true;
+//                                                          options.SignIn.RequireConfirmedEmail = false;
+//                                                          options.SignIn.RequireConfirmedPhoneNumber = false;
+//                                                      }
+//                                                     )
+//                    .AddRoles<IdentityRole>()
+//                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = ShibbolethDefaults.AuthenticationScheme;
+}
+ )
+     .AddUWShibboleth(authenticationScheme: ShibbolethDefaults.AuthenticationScheme, displayName: "shibboleth", options =>
+     {
+         options.UseChallenge = true;  // required to process the Shibboleth login
+         options.CallbackPath = new Microsoft.AspNetCore.Http.PathString("/secure");
+         //options.ProcessChallenge = true;   // readonly on v8
+         //options.ChallengePath = new Microsoft.AspNetCore.Http.PathString("/secure");  // readonly on v8 // must match Path in shibboleth2.xml
+     });
+
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
